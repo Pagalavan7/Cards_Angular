fd2ac08fea53e7e58a211d7d154655da1e1ac58a
Pagalavan7 <p.vetrivendhan@accenture.com>
2024-07-25 13:31:01 +0530

    Cards complete project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, CardsComponent],
 })
 export class AppComponent {
-  title = 'Cards';
+  Posts = [
+    {
+      title: 'Tree',
+      id: '@Nature',
+      description: 'Lorem ipsum dolor sit amet.',
+      image: './assets/tree.jpeg',
+    },
+    {
+      title: 'Biking',
+      id: '@Travel',
+      description: 'Lorem ipsum dolor sit amet.',
+      image: './assets/biking.jpeg',
+    },
+    {
+      title: 'Mountain',
+      id: '@Climbing',
+      description: 'Lorem ipsum dolor sit amet.',
+      image: './assets/mountain.jpeg',
+    },
+  ];
 }
